21a6825c3d1515c8b0e1aaced928e9aa856237af
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2023-12-22 15:16:48 +0900

    xrgamepad

--- plugins/gamepad.js
+++ plugins/gamepad.js
@@ -2,27 +2,50 @@ var create_plugin = (function() {
 	var m_plugin_host = null;
 	var m_is_init = false;
 
-	window.addEventListener('gamepadconnected', function(e) {
-		active_gamepad = e.gamepad.id;
-		console.log("gamepadconnected", active_gamepad);
+	var PUSH_THRESHOLD = 0.5;
+	var m_gamepad_id = "";//need to call navigator.getGamepads() dynamically to get live values
+	var m_gamepad_state = null;
+
+	function get_last_gamepad_id(){
+		if (navigator.getGamepads) {
+			var gamepads = navigator.getGamepads();
+			for (var i=gamepads.length-1;i>=0;i--) {
+				if (gamepads[i]) {
+					return gamepads[i].id;
+				}
+			}
+		}
+		return "";
+	}
+
+	window.addEventListener('gamepadconnected', function (e) {
+		console.log("gamepadconnected : ", e.gamepad.id);
+		m_gamepad_id = e.gamepad.id;
+		console.log("active gamepad id : ", m_gamepad_id);
 	}, false);
-	window.addEventListener('gamepaddisconnected', function(e) {
-		console.log("gamepaddisconnected", active_gamepad);
-		active_gamepad = "";
+	window.addEventListener('gamepaddisconnected', function (e) {
+		console.log("gamepaddisconnected : ", e.gamepad.id);
+		if(e.gamepad.id == m_gamepad_id){
+			m_gamepad_id = get_last_gamepad_id();
+			console.log("active gamepad id : ", m_gamepad_id);
+		}
 	}, false);
 
-	var push_threshold = 0.5;
-	var active_gamepad = "";
-	var gamepads = [];
-	var gamepad_state = null;
-	function handleGamepad() {
-		// Iterate over all the gamepads and show their values.
+	function handleGamepad(callback) {
+		if(!m_gamepad_id){
+			var id = get_last_gamepad_id();
+			if(id){
+				m_gamepad_id = id;
+				console.log("active gamepad id : ", m_gamepad_id);
+			}
+		}
+		var gamepads = [];
 		if (navigator.getGamepads) {
 			gamepads = navigator.getGamepads();
 		}
 		var gamepad = null;
-		for ( var i = 0 in gamepads) {
-			if (gamepads[i] && gamepads[i].id == active_gamepad) {
+		for (var i in gamepads) {
+			if (gamepads[i] && gamepads[i].id == m_gamepad_id) {
 				gamepad = gamepads[i];
 				break;
 			}
@@ -32,27 +55,30 @@ var create_plugin = (function() {
 		}
 
 		var new_state = {}
-		for ( var i = 0 in gamepad.buttons) {
+		for (var i in gamepad.buttons) {
 			var key = i + "_BUTTON";
-			new_state[key] = gamepad.buttons[i].value > push_threshold;
+			new_state[key + "_PUSHED"] = gamepad.buttons[i].value > PUSH_THRESHOLD;
+			new_state[key + "_VALUE"] = gamepad.buttons[i].value;
+			new_state[key + "_PERCENT"] = Math.round(gamepad.buttons[i].value * 100);
 		}
-		for ( var i = 0 in gamepad.axes) {
+		for (var i in gamepad.axes) {
 			var key = i + "_AXIS";
-			new_state[key + "_FORWARD"] = gamepad.axes[i] > push_threshold;
-			new_state[key + "_BACKWARD"] = gamepad.axes[i] < -push_threshold;
+			new_state[key + "_FORWARD"] = gamepad.axes[i] > PUSH_THRESHOLD;
+			new_state[key + "_BACKWARD"] = gamepad.axes[i] < -PUSH_THRESHOLD;
+			new_state[key + "_VALUE"] = gamepad.axes[i];
+			new_state[key + "_PERCENT"] = Math.round(gamepad.axes[i] * 100);
 		}
-		if (gamepad_state) {
-			var eventList = {};
-			for ( var key in new_state) {
-				if (new_state[key] != gamepad_state[key]) {
-					var event = key + "_" + (new_state[key] ? "DOWN" : "UP");
-					if (m_plugin_host) {
-						m_plugin_host.send_event("GAMEPAD", event);
-					}
+		if (!m_gamepad_state) {
+			m_gamepad_state = new_state;
+		}
+		for (var key in new_state) {
+			if (new_state[key] != m_gamepad_state[key]) {
+				if (m_plugin_host) {
+					m_plugin_host.send_event("GAMEPAD", key, new_state);
 				}
 			}
 		}
-		gamepad_state = new_state;
+		m_gamepad_state = new_state;
 	}
 
 	function init() {
