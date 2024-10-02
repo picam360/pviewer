543f4cecf06a6651e548b025b911dfe0b2305757
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2024-10-02 14:56:47 +0900

    wheel

--- plugins/vehicle/wheel.js
+++ plugins/vehicle/wheel.js
@@ -1,12 +1,54 @@
 var create_plugin = (function() {
 	var m_plugin_host = null;
 	var m_is_init = false;
+	var m_gamepad_state = {};
 
 	var SERVER_DOMAIN = UPSTREAM_DOMAIN;
 	var CAPTURE_DOMAIN = SERVER_DOMAIN + UPSTREAM_DOMAIN;
 	var VEHICLE_DOMAIN = SERVER_DOMAIN + "wheel.";
 
 	function init() {
+		setInterval(() => {
+			var cmd = "";
+			let foward_gain = 0.0;
+			{
+				let gamepad = m_gamepad_state["3_AXIS_PERCENT"] || 0;
+				let quest = m_gamepad_state["3_AXIS_PERCENT"] || 0;
+				if(Math.abs(gamepad) > Math.abs(quest)){
+					foward_gain = gamepad;
+				}else{
+					foward_gain = quest;
+				}
+			}
+			let turn_gain = 0.0;
+			{
+				let gamepad = m_gamepad_state["2_AXIS_PERCENT"] || 0;
+				let quest = m_gamepad_state["2_AXIS_PERCENT"] || 0;
+				if(Math.abs(gamepad) > Math.abs(quest)){
+					turn_gain = gamepad;
+				}else{
+					turn_gain = quest;
+				}
+			}
+			if(Math.abs(foward_gain) > Math.abs(turn_gain)){
+				if(foward_gain < -50){
+					cmd = "move_forward";
+				}else if(foward_gain > 50){
+					cmd = "move_backward";
+				}else{
+					cmd = "stop";
+				}
+			}else{
+				if(turn_gain < -50){
+					cmd = "turn_left";
+				}else if(turn_gain > 50){
+					cmd = "turn_right";
+				}else{
+					cmd = "stop";
+				}
+			}
+			m_plugin_host.send_command(cmd);
+		}, 100);
 	}
 	return function(plugin_host) {
 		m_plugin_host = plugin_host;
@@ -14,8 +56,6 @@ var create_plugin = (function() {
 			m_is_init = true;
 			init();
 		}
-		var menu_visible = false;
-		var stereo_enabled = false;
 		var plugin = {
 			event_handler : (sender, key, new_state) => {
 				if(!new_state){//fail safe
@@ -24,80 +64,7 @@ var create_plugin = (function() {
 				if(!new_state[key]){//only push
 					return;
 				}
-				var cmd = "";
-				switch(key){
-					case "10_BUTTON_PUSHED":
-						break;
-					case "11_BUTTON_PUSHED":
-						break;
-					case "3_AXIS_PERCENT":
-						if(new_state[key] < -50){
-							cmd = "MOVE_FORWARD";
-						}else if(new_state[key] > 50){
-							cmd = "MOVE_BACKWARD";
-						}else{
-							cmd = "STOP";
-						}
-						break;
-					case "2_AXIS_PERCENT":
-						if(new_state[key] < -50){
-							cmd = "TURN_LEFT";
-						}else if(new_state[key] > 50){
-							cmd = "TURN_RIGHT";
-						}else{
-							cmd = "STOP";
-						}
-						break;
-					//quest touch : 3_BUTTON stick, 4_BUTTON A, 5_BUTTON B
-					case "LEFT_3_BUTTON_PUSHED":
-						if(new_state[key]){
-						}
-						break;
-					case "RIGHT_3_BUTTON_PUSHED":
-						if(new_state[key]){
-						}
-						break;
-					case "RIGHT_3_AXIS_PERCENT":
-						if(new_state[key] < -50){
-							cmd = "MOVE_FORWARD";
-						}else if(new_state[key] > 50){
-							cmd = "MOVE_BACKWARD";
-						}else{
-							cmd = "STOP";
-						}
-						break;
-					case "RIGHT_2_AXIS_PERCENT":
-						if(new_state[key] < -50){
-							cmd = "TURN_LEFT";
-						}else if(new_state[key] > 50){
-							cmd = "TURN_RIGHT";
-						}else{
-							cmd = "STOP";
-						}
-						break;
-				}
-				switch (cmd) {
-				case "MOVE_FORWARD":
-					var cmd = VEHICLE_DOMAIN + "move_forward";
-					m_plugin_host.send_command(cmd);
-					break;
-				case "MOVE_BACKWARD":
-					var cmd = VEHICLE_DOMAIN + "move_backward";
-					m_plugin_host.send_command(cmd);
-					break;
-				case "TURN_LEFT":
-					var cmd = VEHICLE_DOMAIN + "turn_left";
-					m_plugin_host.send_command(cmd);
-					break;
-				case "TURN_RIGHT":
-					var cmd = VEHICLE_DOMAIN + "turn_right";
-					m_plugin_host.send_command(cmd);
-					break;
-				case "STOP":
-					var cmd = VEHICLE_DOMAIN + "stop";
-					m_plugin_host.send_command(cmd);
-					break;
-				}
+				m_gamepad_state = new_state;
 			},
 		};
 		return plugin;
