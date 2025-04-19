524ca1cab8050d91a25c6438cbb2f64bfe18b8f6
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2025-04-19 21:04:20 +0900

    wrtc

--- js/constants.js
+++ js/constants.js
@@ -12,5 +12,5 @@ var PT_MT_SET_PARAM = 121;
 var P2P_API_KEY = "v8df88o1y4zbmx6r";
 var PUBLIC_VIEWER = "http://picam360.github.io/picam360-viewer/";
 var SIGNALING_HOST = "wrtc-pf.picam360.com";
-var SIGNALING_PORT = 3600;
-var SIGNALING_SECURE = false;
+var SIGNALING_PORT = 36443;
+var SIGNALING_SECURE = true;
